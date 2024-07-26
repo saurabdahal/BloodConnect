8bff5da9f2138e0c73eb5bed0be662a0b200ea6a
Bibas Bhujel <A00298911@mycambrian.ca>
2024-07-26 19:57:03 -0400

    The BloodBank model contains essential informations for identification and contact. The field BloodBankType refers to public or private. This will helps to manage and track blood banks.

---
+++ Models/BloodBank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnect.Models
+{
+    internal class BloodBank
+    {
+        public string BloodBankId { get; set; }
+        public string BloodBankName { get; set; }
+        public string BloodBankType { get; set; }
+        public string BloodBankEmail { get; set; }
+        public string BloodBankPhone { get; set; }
+        public string BloodBankContactPerson { get; set; }
+        public string BloodBankAddress { get; set; }
+    }
+}
+}
