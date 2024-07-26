adb7de96d15c715e568fe6dd80cc513143b89d7a
saurav dahal <d.saurab1@gmail.com>
2024-07-26 18:57:27 -0400

    feat: added model for Receiver. This model is responsible to manage the details for the receiver. It contains fields for name, address, phone, emerggencyphone, address and bloodgroup. Later we can use this model to create history for the donor to use on the dahboard. The donor Model supports similar features for the Donor

---
+++ Models/Receiver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnect.Models
+{
+    internal class Receiver
+    {
+        public string ReceiverId { get; set }
+        public string ReceiverName { get; set; }
+        public int ReceiverAge { get; set; }
+        public string ReceiverEmail { get; set; }
+        public string ReceiverPhone { get; set; }
+        public string ReceiverEmergencyContact { get; set; }
+        public string ReceiverBloodGroup { get; set; }
+        public string ReceiverAddress { get; set; }
+    }
+}
