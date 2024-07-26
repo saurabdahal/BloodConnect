5dea0b1aff009934a2fa489a700110f8ca393ee4
Bibas Bhujel <A00298911@mycambrian.ca>
2024-07-26 19:49:56 -0400

    This model plays the intermediary role for the blood donation system. It includes fields like Mediator Id, Name, Type, Email, Phone and Address. This will facilitates the management of mediators within the system.

---
+++ Models/Mediator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnect.Models
+{
+    internal class Mediator
+    {
+        public string MediatorId { get; set; }
+        public string MediatorName { get; set; }
+        public string MediatorType { get; set; }
+        public string MediatorEmail { get; set; }
+        public string MediatorPhone { get; set;}
+        public string MediatorContactPerson { get; set; }
+        public string MediatorAddress { get;set;}
+    }
+}
