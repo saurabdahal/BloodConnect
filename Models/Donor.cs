39f14c060b877e6696b2a48df685640da37f6b48
saurav dahal <d.saurab1@gmail.com>
2024-07-26 17:28:41 -0400

    Donor model supports similar features for the Donor

--- Models/Donor.cs
+++ Models/Donor.cs
@@ -8,5 +8,13 @@ namespace BloodConnect.Models
 {
     internal class Donor
     {
+        public string DonorId { get; set }
+        public string DonorName { get; set; }
+        public int DonorAge { get; set; }
+        public string DonorEmail { get; set; }
+        public string DonorPhone { get; set; }
+        public string DonorEmergencyContact {get; set;}
+        public string DonorBloodGroup { get;set; }
+        public string DonorAddress { get; set; }
     }
 }
